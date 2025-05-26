521c6bd68cd8b284077b2d01b0d2b4f31c6f1e52
JaydenWN <123134488+JaydenWN@users.noreply.github.com>
2025-05-26 15:21:49 +1000

    changed config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
+	site: "https://thesqueegeeswindowcleaning.com/",
 	integrations: [mdx(), sitemap()],
 	vite: {
 		resolve: {
